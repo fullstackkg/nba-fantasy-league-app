46f40a278a68459c0ecd6e8c24c5212a6c16eaa8
fullstackkg <khalilhgayle@gmail.com>
2024-10-20 18:56:38 -0400

    Initial commit

---
+++ src/main/java/com/nfl/nbafantasyleagueapp/NbaFantasyLeagueAppApplication.java
@@ -0,0 +1,13 @@
+package com.nfl.nbafantasyleagueapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaFantasyLeagueAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbaFantasyLeagueAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nfl/nbafantasyleagueapp/NbaFantasyLeagueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfl.nbafantasyleagueapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaFantasyLeagueAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
